d048aec1be32852c651ac05a577ae198f04ecb7e
cucphuong268 <cucphuong268@gmail.com>
2026-06-25 21:24:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ def run_hrm_analysis():
             comp_allele1_3to5 = get_complement_3to5(allele1)
             comp_allele2_3to5 = get_complement_3to5(allele2)
             try:
-                Tm_het1 = mt.Tm_NN(allele1, c_seq=comp_allele2_3to5, nn_table=mt.DNA_NN4, dnac1=dnac1_nm, dnac2=dnac2_nm, Na=na_mM, Mg=mg_mM, dNTPs=dNTPs_mM, saltcorr=7)
-                Tm_het2 = mt.Tm_NN(allele2, c_seq=comp_allele1_3to5, nn_table=mt.DNA_NN4, dnac1=dnac1_nm, dnac2=dnac2_nm, Na=na_mM, Mg=mg_mM, dNTPs=dNTPs_mM, saltcorr=7)
+                Tm_het1 = mt.Tm_NN(allele1, c_seq=comp_allele2_3to5, nn_table=mt.DNA_TMM,dnac1=dnac1_nm, dnac2=dnac2_nm, Na=na_mM, Mg=mg_mM, dNTPs=dNTPs_mM, saltcorr=7)
+                Tm_het2 = mt.Tm_NN(allele2, c_seq=comp_allele1_3to5, nn_table=mt.DNA_TMM,dnac1=dnac1_nm, dnac2=dnac2_nm, Na=na_mM, Mg=mg_mM, dNTPs=dNTPs_mM, saltcorr=7)
                 penalty_1, penalty_2 = Tm1 - Tm_het1, Tm2 - Tm_het2
             except:
                 penalty_1, penalty_2 = 1.5, 1.5
